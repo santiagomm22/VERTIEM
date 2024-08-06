6458a650e748b14d65d710f38eef420a641f7f47
santiagomm22 <sntgmm22@gmail.com>
2024-08-06 09:40:27 -0500

    :star:

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -4,7 +4,7 @@ function Card() {
       <div className="col">
         <div className="card border-0">
           <img
-            src="./images/imgprueba/img1.jpg"
+            src="https://concepto.de/wp-content/uploads/2015/03/administrador-e1550360735144.jpg"
             className="card-img-top "
             style={{ borderRadius: "100px" }}
             alt="..."
@@ -22,7 +22,7 @@ function Card() {
       <div className="col">
         <div className="card border-0">
           <img
-            src="./images/imgprueba/img2.jpg"
+            src="https://media.istockphoto.com/id/1372336145/es/vector/peque%C3%B1a-empresa-y-tienda-local-con-concepto-de-tienda-boutique-frente-a-la-peque%C3%B1a-persona.jpg?s=612x612&w=0&k=20&c=qdpEr0jqk2RdusQMOi6mvVRlzwXxgNLRYrIhXr3AA8k="
             className="card-img-top"
             style={{ borderRadius: "100px" }}
             alt="..."
